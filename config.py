2c28b5194a883342bd290bcb4648b80ea8f07f84
Toxic <gyaneshp606@gmail.com>
2026-02-28 18:28:00 +0530

    I

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(getenv("API_ID", "39934421"))
 API_HASH = getenv("API_HASH", "407aaa4db92ab5d0b32027d6482a5fff")
 BOT_TOKEN = getenv("BOT_TOKEN", "8646766746:AAHkoZKK9YuyOkBBcOMzECzbg0UHYdkEs4M")
 
-MONGO_URI = getenv("MONGO_URI", "mongodb+srv://odduscode02_db_user:6Ldc6lxc4DWLkfzR@cluster0.8azyhaj.mongodb.net/?appName=Cluster0")
+MONGO_URI = getenv("MONGO_URI", "")
 
 ADMINS = [6539720810, 7634311488, 8238387029]
 STATIC_2FA_PASSWORD = "dadatg"
