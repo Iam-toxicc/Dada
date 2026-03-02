2fed1d59a1924fe8219b7ea2ddc9cd327fbf4e2d
Toxic <gyaneshp606@gmail.com>
2026-03-02 22:14:23 +0530

    Iam-Toxicc

--- plugins/buy.py
+++ plugins/buy.py
@@ -315,12 +315,12 @@ async def send_public_log(client, user, country, price, item_data, flag):
             f"<b>✚ <u>OTP</u>:</b>  <spoiler>******</spoiler> 💬\n"
             f"<b>✚ <u>Server</u>:</b>  <b>(1)</b> 🥂\n"
             f"<b>✚ <u>Password</u>:</b>  <spoiler>{STATIC_2FA_PASSWORD}</spoiler> 🔐\n\n"
-            f"<b>✦</b> <i>@DADA_OTP_02bot</i>  <b>||</b>  <i>@dada_store_sms</i> <b>✦</b>"
+            f"<b>✦</b> <i>@OTP_DADA_SMS_BOT</i>  <b>||</b>  <i>@dada_store_sms</i> <b>✦</b>"
         )
         
         # Hardcoded URL for optimization
         buttons = InlineKeyboardMarkup([
-            [InlineKeyboardButton("🛒 • Buy Now • 🛒", url="https://t.me/DADA_OTP_CHENNAL")]
+            [InlineKeyboardButton("🛒 • Buy Now • 🛒", url="https://t.me/OTP_DADA_SMS_BOT")]
         ])
         
         await client.send_message(
