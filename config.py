5153df79854574900be3db55cf2245ec725c8182
Toxic <gyaneshp606@gmail.com>
2026-03-03 10:11:10 +0530

    Iam-Toxicc

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "33881397")) 
 API_HASH = getenv("API_HASH", "c357c5553e67ccbaeb85fa3cfcb2f9cb")
-BOT_TOKEN = getenv("BOT_TOKEN", "8326002491:AAGBhmVUge2m0sWSRIclGn9hcmp8Er1GNto")
+BOT_TOKEN = getenv("BOT_TOKEN", "8326002491:AAF644YebHi9Os8U_5gSaE9wenvcmCAmphY")
 
 MONGO_URI = getenv("MONGO_URI", "mongodb+srv://shourya6055_db_user:Ngt1JdbWoFXhJ9RX@toxic.lsnylyq.mongodb.net/?appName=Toxic")
 
